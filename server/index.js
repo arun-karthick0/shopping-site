be194063a26ad814c3a03f443208c1ea715a0f16
Arun <arunkarthick0810@gmail.com>
2023-08-10 18:42:11 +0530

    update

--- server/index.js
+++ server/index.js
@@ -35,8 +35,8 @@ app.post("/create-checkout-session", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items,
     mode: "payment",
-    success_url: "http://localhost:3000/cart/success",
-    cancel_url: "http://localhost:3000/cart/cancel",
+    success_url: "https://shopping-site-001.netlify.app/cart/success",
+    cancel_url: "https://shopping-site-001.netlify.app/cart/cancel",
   });
 
   try {
